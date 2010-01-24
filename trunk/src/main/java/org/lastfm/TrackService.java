f0fe32883647e1fee1e594a22c7cba43d9eac0d0
joseluis.delacruz <joseluis.delacruz@8463f722-03e2-11df-8b0e-05a2803efa1e>
2010-01-24 20:15:01 +0000

    Add get trackNumber functionality to trackService

--- trunk/src/main/java/org/lastfm/TrackService.java
+++ trunk/src/main/java/org/lastfm/TrackService.java
@@ -1,6 +1,8 @@
 package org.lastfm;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.slychief.javamusicbrainz.ServerUnavailableException;
 import com.slychief.javamusicbrainz.entities.Release;
@@ -9,8 +11,11 @@ import com.slychief.javamusicbrainz.entities.Track;
 public class TrackService extends Track {
 	List<Track> trackList;
 	List<Release> release;
+	Map<String, Integer> compilation = new HashMap<String, Integer>();
 
-	public String getAlbum(String artistName, String trackName) throws ServerUnavailableException {
+	public String getAlbum(String artistName, String trackName)
+			throws ServerUnavailableException {
+		System.out.println("--------------------------------------------");
 		String album = "";
 		trackList = Track.findByTitle(trackName);
 		if (!trackList.isEmpty()) {
@@ -19,14 +24,20 @@ public class TrackService extends Track {
 				if (artistName.equals(track.getArtist().getName())) {
 					System.out.println(track.getArtist().getName());
 					release = track.getReleases().getReleases();
-					for (Release rel : release) {
-						album = rel.getTitle();
-						System.out.println(album);
-					}
+					String trackNumber = (release.get(0).getTrackList().getOffset()).trim();
+					System.out.println(Integer.parseInt(trackNumber) + 1);
+					album = release.get(0).getTitle();
+					compilation.put(album, Integer.parseInt(trackNumber));
+					break;
 				}
 			}
 		}
 		return album;
+	}
+
+	public int getTrackNumber(String album) {
+		//TrackList offset starts in 0 
+		return compilation.get(album) + 1;
 	}
 
 }
