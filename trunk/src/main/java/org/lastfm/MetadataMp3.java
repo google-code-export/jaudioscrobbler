734793ec81f0af3f5a47e999a35682d174066235
joseluis.delacruz <joseluis.delacruz@8463f722-03e2-11df-8b0e-05a2803efa1e>
2010-01-28 03:14:22 +0000

    Increment test coverage to MetadataMP3

--- trunk/src/main/java/org/lastfm/MetadataMp3.java
+++ trunk/src/main/java/org/lastfm/MetadataMp3.java
@@ -1,33 +1,53 @@
 package org.lastfm;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.jaudiotagger.audio.AudioFile;
-import org.jaudiotagger.audio.AudioFileIO;
 import org.jaudiotagger.audio.AudioHeader;
 import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.CannotWriteException;
 import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
 import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
 import org.jaudiotagger.audio.mp3.MP3File;
 import org.jaudiotagger.tag.FieldKey;
 import org.jaudiotagger.tag.Tag;
 import org.jaudiotagger.tag.TagException;
+import org.jaudiotagger.tag.id3.AbstractID3v2Tag;
+import org.jaudiotagger.tag.id3.ID3v24Tag;
 
 
 public class MetadataMp3 extends Metadata {
 	private Tag tag;
 	private AudioHeader header;
-	private final File file;
+	private File file;
+	private AudioFile audioFile;
 	
-
-	public MetadataMp3(File file) throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException, InvalidId3VersionException {
+	public MetadataMp3(File file) throws FileNotFoundException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException, InvalidId3VersionException {
 		this.file = file;
-		AudioFile audioFile = AudioFileIO.read(file);
+		getMetadata();
+	}
+	
+	//For Mocking proposes
+	public MetadataMp3(File file, AudioFile audioFile) throws FileNotFoundException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException, InvalidId3VersionException {
+		this.audioFile = audioFile; 
+		this.file = file;
+		getMetadata();
+	}
+
+	public void getMetadata() throws CannotReadException, IOException, TagException, ReadOnlyFileException, InvalidAudioFrameException, InvalidId3VersionException {
 		if (audioFile instanceof MP3File) {
-			MP3File audioFMP3 = (MP3File) audioFile;
-			if (!audioFMP3.hasID3v2Tag()) {
-				throw new InvalidId3VersionException(); 
+			MP3File audioMP3 = (MP3File) audioFile;
+			if (!audioMP3.hasID3v2Tag()) {
+				AbstractID3v2Tag id3v2tag = new ID3v24Tag();
+				audioMP3.setID3v2TagOnly(id3v2tag);
+				try {
+					audioFile.commit();
+				} catch (CannotWriteException cwe) {
+					this.getLog().error("An error occurs when I tried to update to ID3 v2");
+					cwe.printStackTrace();
+				}
 			} else {
 				tag = audioFile.getTag();
 				header = audioFile.getAudioHeader();
