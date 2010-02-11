95a669ebb643a45afdb2a91eb92eda0e54ec5e1c
zageyiff@gmail.com <zageyiff@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2010-02-11 23:55:13 +0000

    Set Nimbus look and feel

--- src/main/java/org/lastfm/Launcher.java
+++ src/main/java/org/lastfm/Launcher.java
@@ -1,10 +1,31 @@
 package org.lastfm;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 import org.lastfm.gui.LoginWindow;
 import org.lastfm.gui.MainWindow;
 
 public class Launcher {
 	public static void main(String[] args) {
+		try {
+			for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+				if ("Nimbus".equals(info.getName())) {
+					UIManager.setLookAndFeel(info.getClassName());
+					break;
+				}
+			}
+		} catch (UnsupportedLookAndFeelException e) {
+			// handle exception
+		} catch (ClassNotFoundException e) {
+			// handle exception
+		} catch (InstantiationException e) {
+			// handle exception
+		} catch (IllegalAccessException e) {
+			// handle exception
+		}
+		
 		new ApplicationState();
 		new ScrobblerController(new HelperScrobbler(), new MainWindow(), new LoginWindow());
 	}
