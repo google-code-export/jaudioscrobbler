b90ade70c80c64a99aee6e0fbcb8fa5632e2f317
joseluis.delacruz <joseluis.delacruz@8463f722-03e2-11df-8b0e-05a2803efa1e>
2010-02-12 05:12:27 +0000

    move getMetadataList to ScrobblerController

--- src/main/java/org/lastfm/HelperScrobbler.java
+++ src/main/java/org/lastfm/HelperScrobbler.java
@@ -2,7 +2,6 @@ package org.lastfm;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,15 +12,9 @@ import net.roarsoftware.lastfm.scrobble.Source;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
-import org.jaudiotagger.audio.exceptions.CannotReadException;
-import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
-import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
-import org.jaudiotagger.tag.TagException;
 
 public class HelperScrobbler {
 	private static final int DELTA = 120;
-	private static final int MARGIN = 10;
-	private int pendingFiles;
 
 	Map<Metadata, Long> metadataMap;
 
@@ -59,32 +52,6 @@ public class HelperScrobbler {
 		}
 	}
 
-	public List<Metadata> getMetadataList(List<File> fileList) throws InterruptedException, IOException,
-			CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException,
-			InvalidId3VersionException {
-		this.fileList = fileList;
-		List<Metadata> metadataList;
-		Metadata metadata = null;
-		metadataList = new ArrayList<Metadata>();
-
-		for (File file : fileList) {
-			if (file.getPath().endsWith("mp3")) {
-				metadata = new MetadataMp3(file);
-			} else if (file.getPath().endsWith("m4a")) {
-				metadata = new MetadataMp4(file);
-			}
-
-			if (metadata == null) {
-				log.error(file.getAbsoluteFile() + " is not a valid Audio File");
-			} else if (StringUtils.isNotEmpty(metadata.getArtist()) && StringUtils.isNotEmpty(metadata.getTitle())) {
-				metadataList.add(metadata);
-				ApplicationState.update(metadata);
-			}
-		}
-		pendingFiles = metadataList.size() + MARGIN;
-		return metadataList;
-	}
-
 	public int send(Metadata metadata) throws IOException, InterruptedException {
 		int result = ApplicationState.FAILURE;
 		long time = (System.currentTimeMillis() / 1000);
@@ -92,10 +59,9 @@ public class HelperScrobbler {
 		// According to Submission rules http://www.last.fm/api/submissions
 		if (StringUtils.isNotEmpty(metadata.getArtist()) && StringUtils.isNotEmpty(metadata.getTitle())
 				&& metadata.getLength() > 240) {
-			long startTime = time - (pendingFiles * DELTA);
+			long startTime = time - (metadataMap.size() * DELTA);
 			metadataMap.put(metadata, startTime);
 			result = scrobbling(metadata);
-			pendingFiles--;
 		}
 		return result;
 	}
