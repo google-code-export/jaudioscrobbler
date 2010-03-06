4b372b0616d7aa68a9446f12d989389cc9ed609d
joseluis.delacruz <joseluis.delacruz@8463f722-03e2-11df-8b0e-05a2803efa1e>
2010-03-06 16:43:44 +0000

    Register keyStroke action on OpenButton

--- trunk/src/test/java/org/lastfm/gui/TestMainWindow.java
+++ trunk/src/test/java/org/lastfm/gui/TestMainWindow.java
@@ -1,14 +1,26 @@
 package org.lastfm.gui;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TestMainWindow {
 	private MainWindow mainWindow;
-	
+
+	@Before
+	public void setup() {
+		mainWindow = new MainWindow();
+	}
+
 	@After
 	public void finalize(){
 		mainWindow.getFrame().dispose();
@@ -16,9 +28,19 @@ public class TestMainWindow {
 
 	@Test
 	public void shouldNotEnableSendAndCompleteButton() throws Exception {
-		mainWindow = new MainWindow();
 		assertFalse("sendButton should not be enabled", mainWindow.getSendButton().isEnabled());
 		assertFalse("completeButton should not be enabled", mainWindow.getCompleteButton().isEnabled());
 		assertFalse("descriptionTable should be disabled", mainWindow.getDescriptionTable().isEnabled());
+	}
+	
+	@Test
+	public void shouldVerifyKeyStrokeActionWasRegistered() throws Exception {
+		assertEquals(2, mainWindow.inputMap.size());
+		assertEquals(2, mainWindow.openButton.getActionMap().size());
+		
+		KeyStroke ctrlo = KeyStroke.getKeyStroke(KeyEvent.VK_O, InputEvent.CTRL_DOWN_MASK);
+		KeyStroke enter = KeyStroke.getKeyStroke(KeyEvent.VK_ENTER, 0);
+		assertEquals(MainWindow.CTRL_O, mainWindow.inputMap.get(ctrlo));
+		assertEquals(MainWindow.ENTER, mainWindow.inputMap.get(enter));
 	}
 }
