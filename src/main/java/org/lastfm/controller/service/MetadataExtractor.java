e6f10bc13c359af9a0eecdaaa83535cba230b489
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-08-24 23:02:40 +0000

    clean code

---
+++ src/main/java/org/lastfm/controller/service/MetadataExtractor.java
@@ -0,0 +1,67 @@
+package org.lastfm.controller.service;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
+import org.lastfm.action.control.ControlEngineConfigurator;
+import org.lastfm.event.Events;
+import org.lastfm.event.ValueEvent;
+import org.lastfm.exception.InvalidId3VersionException;
+import org.lastfm.metadata.Metadata;
+import org.lastfm.metadata.MetadataException;
+import org.lastfm.metadata.Mp3Reader;
+import org.lastfm.metadata.Mp4Reader;
+import org.lastfm.util.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author josdem (joseluis.delacruz@gmail.com)
+ * @understands A class who know how extract metadata from files using a root directory
+ */
+
+@Service
+public class MetadataExtractor {
+	private List<Metadata> metadataList = new ArrayList<Metadata>();
+	private FileUtils fileUtils = new FileUtils();
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	@Autowired
+	private ControlEngineConfigurator configurator;
+
+	public List<Metadata> extractMetadata(File root) throws InterruptedException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException, InvalidId3VersionException, MetadataException {
+		List<File> fileList = fileUtils.getFileList(root);
+		return getMetadataList(fileList);
+	}
+
+	private List<Metadata> getMetadataList(List<File> fileList) throws InterruptedException, IOException, CannotReadException, TagException, ReadOnlyFileException, InvalidAudioFrameException,
+			InvalidId3VersionException, MetadataException {
+
+		Metadata metadata = null;
+		for (File file : fileList) {
+			if (file.getPath().endsWith("mp3")) {
+				metadata = new Mp3Reader().getMetadata(file);
+			} else if (file.getPath().endsWith("m4a")) {
+				metadata = new Mp4Reader().getMetadata(file);
+			}
+
+			if (metadata == null) {
+				log.error(file.getAbsoluteFile() + " is not a valid Audio File");
+			} else if (StringUtils.isNotEmpty(metadata.getArtist()) && StringUtils.isNotEmpty(metadata.getTitle())) {
+				metadataList.add(metadata);
+				configurator.getControlEngine().fireEvent(Events.LOAD, new ValueEvent<Metadata>(metadata));
+			}
+		}
+		return metadataList;
+	}
+
+}
