fc6bec6d48826cf58cc1db0efb861727794d3018
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-10-15 15:15:06 +0000

    Adding send and apply workers testing using FEST

--- src/test/java/org/lastfm/gui/TestMainWindow.java
+++ src/test/java/org/lastfm/gui/TestMainWindow.java
@@ -1,16 +1,24 @@
 package org.lastfm.gui;
 
 import static org.junit.Assert.assertFalse;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Matchers.isA;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.fest.swing.fixture.FrameFixture;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.lastfm.action.Actions;
+import org.lastfm.action.ResponseCallback;
 import org.lastfm.action.ViewEngine;
 import org.lastfm.action.control.ViewEngineConfigurator;
+import org.lastfm.metadata.Metadata;
+import org.lastfm.model.Model;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -20,11 +28,15 @@ public class TestMainWindow {
 	private MainWindow mainWindow = new MainWindow();
 	private FrameFixture window;
 	private static final String OPEN_BUTTON_NAME = "openButton";
+	private static final String SEND_BUTTON_NAME = "sendButton";
+	private static final String APPLY_BUTTON_NAME = "applyButton";
 	
 	@Mock
 	private ViewEngineConfigurator configurator;
 	@Mock
 	private ViewEngine viewEngine;
+	@Mock
+	private Metadata metadata;
 	
 	@Before
 	public void setup() throws Exception {
@@ -42,6 +54,36 @@ public class TestMainWindow {
 		assertFalse(mainWindow.getSendButton().isEnabled());
 		assertFalse(mainWindow.getCompleteMetadataButton().isEnabled());
 		assertFalse(mainWindow.getApplyButton().isEnabled());
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldSend() throws Exception {
+		List<Metadata> metadatas = setMetadataListExpectations();
+		when(viewEngine.get(Model.METADATA)).thenReturn(metadatas);
+		mainWindow.getSendButton().setEnabled(true);
+		
+		window.button(SEND_BUTTON_NAME).click();
+		
+		verify(viewEngine).request(eq(Actions.SEND), eq(metadata), isA(ResponseCallback.class));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldApply() throws Exception {
+		List<Metadata> metadatas = setMetadataListExpectations();
+		when(viewEngine.get(Model.METADATA_ARTIST)).thenReturn(metadatas);
+		mainWindow.getApplyButton().setEnabled(true);
+		
+		window.button(APPLY_BUTTON_NAME).click();
+		
+		verify(viewEngine).request(eq(Actions.WRITE), eq(metadata), isA(ResponseCallback.class));
+	}
+
+	private List<Metadata> setMetadataListExpectations() {
+		List<Metadata> metadatas = new ArrayList<Metadata>();
+		metadatas.add(metadata);
+		return metadatas;
 	}
 	
 	@After
