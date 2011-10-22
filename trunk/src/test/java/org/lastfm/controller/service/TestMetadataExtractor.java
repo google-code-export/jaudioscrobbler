873e8acfd7879ef00accbf723eb452cf1adab03f
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-10-22 15:49:46 +0000

    Bug 5 fixed: "Track loaded in Description table w/sfv files". Cover it with test shouldDetectANotValidAudioFile in TestMetadataExtractor.

--- trunk/src/test/java/org/lastfm/controller/service/TestMetadataExtractor.java
+++ trunk/src/test/java/org/lastfm/controller/service/TestMetadataExtractor.java
@@ -1,34 +1,36 @@
 package org.lastfm.controller.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.eq;
-import static org.mockito.Matchers.isA;
-import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.jaudiotagger.audio.exceptions.CannotReadException;
+import org.jaudiotagger.audio.exceptions.InvalidAudioFrameException;
+import org.jaudiotagger.audio.exceptions.ReadOnlyFileException;
+import org.jaudiotagger.tag.TagException;
 import org.junit.Before;
 import org.junit.Test;
 import org.lastfm.action.control.ControlEngine;
 import org.lastfm.action.control.ControlEngineConfigurator;
 import org.lastfm.event.Events;
 import org.lastfm.event.ValueEvent;
+import org.lastfm.exception.InvalidId3VersionException;
 import org.lastfm.metadata.Metadata;
 import org.lastfm.util.FileUtils;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-
 public class TestMetadataExtractor {
 	@InjectMocks
 	private MetadataExtractor metadataExtractor = new MetadataExtractor();
-	
+
 	@Mock
 	private FileUtils fileUtils;
 	@Mock
@@ -37,41 +39,50 @@ public class TestMetadataExtractor {
 	private ControlEngineConfigurator configurator;
 	@Mock
 	private ControlEngine controlEngine;
-	
+
 	private List<File> fileList;
 	private static final int FIRST_ELEMENT = 0;
 	private File audioFile = new File("src/test/resources/audio/Jaytech - Pepe Garden (Original Mix).mp3");
 	private File checkStyleFile = new File("src/test/resources/checkstyle/checkstyle.xml");
-	
+
 	@Before
 	public void setup() throws Exception {
 		MockitoAnnotations.initMocks(this);
 		when(configurator.getControlEngine()).thenReturn(controlEngine);
+		when(fileUtils.isMp3File(audioFile)).thenReturn(true);
 		fileList = new ArrayList<File>();
 	}
-	
+
 	@Test
 	public void shouldExtractMetadata() throws Exception {
 		fileList.add(audioFile);
 		when(fileUtils.getFileList(root)).thenReturn(fileList);
-		
+
 		List<Metadata> metadatas = metadataExtractor.extractMetadata(root);
 		Metadata metadata = metadatas.get(FIRST_ELEMENT);
-		
-		verify(fileUtils).getFileList(root);
-		assertEquals("Jaytech", metadata.getArtist());
-		verify(controlEngine).fireEvent(Events.LOAD, new ValueEvent<Metadata>(metadata));
+
+		verifyExpectations(metadatas, metadata);
 	}
-	
+
 	@Test
 	public void shouldDetectANotValidAudioFile() throws Exception {
+		fileList.add(audioFile);
 		fileList.add(checkStyleFile);
+		when(fileUtils.isMp3File(checkStyleFile)).thenReturn(false);
 		when(fileUtils.getFileList(root)).thenReturn(fileList);
-		
+
 		List<Metadata> metadatas = metadataExtractor.extractMetadata(root);
-		
-		assertTrue(metadatas.isEmpty());
-		verify(fileUtils).getFileList(root);
-		verify(controlEngine, never()).fireEvent(Events.LOAD, new ValueEvent<Metadata>(eq(isA(Metadata.class))));
+		Metadata metadata = metadatas.get(FIRST_ELEMENT);
+
+		verifyExpectations(metadatas, metadata);
 	}
+
+	private void verifyExpectations(List<Metadata> metadatas, Metadata metadata) throws InterruptedException, IOException, CannotReadException, TagException, ReadOnlyFileException,
+			InvalidAudioFrameException, InvalidId3VersionException {
+		assertEquals(1, metadatas.size());
+		verify(fileUtils).getFileList(root);
+		assertEquals("Jaytech", metadata.getArtist());
+		verify(controlEngine, times(1)).fireEvent(Events.LOAD, new ValueEvent<Metadata>(metadata));
+	}
+
 }
