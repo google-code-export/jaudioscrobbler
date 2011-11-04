7ee1b7d67160cf31350247f1fbaf2df76913e8ad
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-11-04 18:09:48 +0000

    Refactor in CoverArt LastFM Service

--- src/main/java/org/lastfm/controller/service/CoverArtService.java
+++ src/main/java/org/lastfm/controller/service/CoverArtService.java
@@ -1,6 +1,5 @@
 package org.lastfm.controller.service;
 
-import java.awt.Image;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -8,50 +7,25 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.lastfm.action.ActionResult;
-import org.lastfm.helper.LastFMAlbumHelper;
+import org.lastfm.helper.CompleteHelper;
 import org.lastfm.metadata.Metadata;
 import org.lastfm.model.LastfmAlbum;
 import org.springframework.stereotype.Service;
 
-import de.umass.lastfm.Album;
-import de.umass.lastfm.ImageSize;
-
 @Service
 public class CoverArtService {
-	private LastFMAlbumHelper helper = new LastFMAlbumHelper();
+	private CompleteHelper completeHelper = new CompleteHelper();
 	private Log log = LogFactory.getLog(this.getClass());
-	
-	private LastfmAlbum getCoverArtFromLastfm(String artist, String album) throws MalformedURLException, IOException{
-		Album info = helper.getAlbum(artist, album);
-		if(info != null){
-			LastfmAlbum lastfmAlbum = new LastfmAlbum();
-			String imageURL = info.getImageURL(ImageSize.EXTRALARGE);
-			log.info("imageURL: " + imageURL + " from album: " + info.getName());
-			if(StringUtils.isEmpty(imageURL)){
-				return null;
-			}
-			Image image = helper.readImage(imageURL);
-			lastfmAlbum.setImageIcon(helper.getImageIcon(image));
-			return lastfmAlbum;
-		} else {
-			return null;
-		}
-	}
 
 	public ActionResult completeCoverArt(Metadata metadata) {
-		try{
-			if(metadata.getCoverArt() == null){
-				if(!StringUtils.isEmpty(metadata.getAlbum()) && !StringUtils.isEmpty(metadata.getArtist())){
-					log.info("Getting Cover Art for Album: " + metadata.getAlbum() + " by " + metadata.getArtist());
-					LastfmAlbum lastfmAlbum = getCoverArtFromLastfm(metadata.getArtist(), metadata.getAlbum());
-					if(lastfmAlbum.getImageIcon() == null){
-						return ActionResult.COVER_ART_ERROR;
-					}
-					metadata.setLastfmCoverArt(lastfmAlbum.getImageIcon());
-					return ActionResult.COVER_ART_SUCCESS;
-				} else {
-					return ActionResult.NOT_ENOUGH_ARGUMENTS;
+		try {
+			if (completeHelper.canLastFMHelpToComplete(metadata)) {
+				LastfmAlbum lastfmAlbum = completeHelper.getCoverArt(metadata);
+				if (lastfmAlbum.getImageIcon() == null) {
+					return ActionResult.COVER_ART_ERROR;
 				}
+				metadata.setLastfmCoverArt(lastfmAlbum.getImageIcon());
+				return ActionResult.COVER_ART_SUCCESS;
 			} else {
 				return ActionResult.METADATA_COMPLETE;
 			}
@@ -64,89 +38,42 @@ public class CoverArtService {
 		} catch (NullPointerException npe) {
 			log.error(npe, npe);
 			return ActionResult.COVER_ART_ERROR;
-		}
-	}
-	
-	private LastfmAlbum getYearFromLastfm(String artist, String album) throws MalformedURLException, IOException{
-		Album info = helper.getAlbum(artist, album);
-		if(info != null){
-			LastfmAlbum lastfmAlbum = new LastfmAlbum();
-			log.info("Year date format: " + info.getReleaseDate());
-			lastfmAlbum.setYear(helper.getYear(info.getReleaseDate()));
-			log.info("Year metadata format: " + lastfmAlbum.getYear());
-			return lastfmAlbum;
-		} else {
-			return null;
 		}
 	}
 
 	public ActionResult completeYearLastfmMetadata(Metadata metadata) {
-		try{
-			if(StringUtils.isEmpty(metadata.getYear())){
-				if(!StringUtils.isEmpty(metadata.getAlbum()) && !StringUtils.isEmpty(metadata.getArtist())){
-					log.info("Getting Year for Album: " + metadata.getAlbum() + " by " + metadata.getArtist());
-					LastfmAlbum lastfmAlbum = getYearFromLastfm(metadata.getArtist(), metadata.getAlbum());
-					if(StringUtils.isEmpty(lastfmAlbum.getYear())){
-						return ActionResult.YEAR_ERROR;
-					}
-					metadata.setYear(lastfmAlbum.getYear());
-					return ActionResult.YEAR_SUCCESS;
-				} else {
-					return ActionResult.NOT_ENOUGH_ARGUMENTS;
+		try {
+			if (completeHelper.canLastFMHelpToComplete(metadata)) {
+				LastfmAlbum lastfmAlbum = completeHelper.getYear(metadata);
+				if (StringUtils.isEmpty(lastfmAlbum.getYear())) {
+					return ActionResult.YEAR_ERROR;
 				}
+				metadata.setYear(lastfmAlbum.getYear());
+				return ActionResult.YEAR_SUCCESS;
 			} else {
 				return ActionResult.METADATA_COMPLETE;
 			}
-		} catch (MalformedURLException mfe) {
-			log.error(mfe, mfe);
-			return ActionResult.YEAR_ERROR;
-		} catch (IOException ioe) {
-			log.error(ioe, ioe);
-			return ActionResult.YEAR_ERROR;
 		} catch (NullPointerException npe) {
 			log.error(npe, npe);
-			return ActionResult.YEAR_ERROR;
-		}
-	}
-	
-	private LastfmAlbum getGenreFromLastfm(String artist, String album) throws MalformedURLException, IOException{
-		Album info = helper.getAlbum(artist, album);
-		if(info != null){
-			LastfmAlbum lastfmAlbum = new LastfmAlbum();
-			lastfmAlbum.setGenre(helper.getGenre(info));
-			return lastfmAlbum;
-		} else {
-			return null;
+			return ActionResult.COVER_ART_ERROR;
 		}
 	}
 
 	public ActionResult completeGenreLastfmMetadata(Metadata metadata) {
-		try{
-			if(StringUtils.isEmpty(metadata.getGenre())){
-				if(!StringUtils.isEmpty(metadata.getAlbum()) && !StringUtils.isEmpty(metadata.getArtist())){
-					log.info("Getting Genre for Album: " + metadata.getAlbum() + " by " + metadata.getArtist());
-					LastfmAlbum lastfmAlbum = getGenreFromLastfm(metadata.getArtist(), metadata.getAlbum());
-					if(StringUtils.isEmpty(lastfmAlbum.getGenre())){
-						return ActionResult.GENRE_ERROR;
-					}
-					metadata.setGenre(lastfmAlbum.getGenre());
-					return ActionResult.GENRE_SUCCESS;
-				} else {
-					return ActionResult.NOT_ENOUGH_ARGUMENTS;
+		try {
+			if (completeHelper.canLastFMHelpToComplete(metadata)) {
+				LastfmAlbum lastfmAlbum = completeHelper.getGenre(metadata);
+				if (StringUtils.isEmpty(lastfmAlbum.getGenre())) {
+					return ActionResult.GENRE_ERROR;
 				}
+				metadata.setGenre(lastfmAlbum.getGenre());
+				return ActionResult.GENRE_SUCCESS;
 			} else {
 				return ActionResult.METADATA_COMPLETE;
 			}
-		} catch (MalformedURLException mfe) {
-			log.error(mfe, mfe);
-			return ActionResult.GENRE_ERROR;
-		} catch (IOException ioe) {
-			log.error(ioe, ioe);
-			return ActionResult.GENRE_ERROR;
 		} catch (NullPointerException npe) {
 			log.error(npe, npe);
-			return ActionResult.GENRE_ERROR;
+			return ActionResult.COVER_ART_ERROR;
 		}
 	}
 }
- 
