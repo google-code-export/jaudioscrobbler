bbb48184a6f03cfc1668b4dc1beb3d5b5438fb4e
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-11-04 16:13:45 +0000

    Add test at match and not match lastFM tags w/GenreTypes

--- trunk/src/test/java/org/lastfm/helper/TestLastFMAlbumHelper.java
+++ trunk/src/test/java/org/lastfm/helper/TestLastFMAlbumHelper.java
@@ -1,16 +1,32 @@
 package org.lastfm.helper;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import de.umass.lastfm.Album;
 
 
 public class TestLastFMAlbumHelper {
 	private LastFMAlbumHelper lastFMAlbumHelper = new LastFMAlbumHelper();
 	private Date releaseDate = new Date();
+	@Mock
+	private Album album;
+	
+	@Before
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
 	
 	@Test
 	public void shouldGetYear() throws Exception {
@@ -18,5 +34,29 @@ public class TestLastFMAlbumHelper {
 		String currentYear = simpleDateformat.format(releaseDate);
 		String year = lastFMAlbumHelper.getYear(releaseDate);
 		assertEquals(currentYear, year);
+	}
+	
+	@Test
+	public void shouldMatchAGenre() throws Exception {
+		Collection<String> tags = new ArrayList<String>();
+		String tag = "House";
+		tags.add(tag);
+		
+		when(album.getTags()).thenReturn(tags);
+		
+		String result = lastFMAlbumHelper.getGenre(album);
+		assertEquals(tag, result);
+	}
+	
+	@Test
+	public void shouldNotMatchAGenre() throws Exception {
+		Collection<String> tags = new ArrayList<String>();
+		String tag = "usa";
+		tags.add(tag);
+		
+		when(album.getTags()).thenReturn(tags);
+		
+		String result = lastFMAlbumHelper.getGenre(album);
+		assertEquals(StringUtils.EMPTY, result);
 	}
 }
