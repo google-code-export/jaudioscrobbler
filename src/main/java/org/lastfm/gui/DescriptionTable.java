2189e0d71952cf95c350acb118fc8a6fd7865aa4
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2011-11-25 23:16:06 +0000

    Modify column size values

--- src/main/java/org/lastfm/gui/DescriptionTable.java
+++ src/main/java/org/lastfm/gui/DescriptionTable.java
@@ -17,8 +17,8 @@ public class DescriptionTable extends JTable{
             "Album",
             "Genre",
             "Year",
-            "# Track",
-            "# Tracks",
+            "# Trk",
+            "# Trks",
             "# CD",
             "# CDs",
             "Status"};
@@ -39,20 +39,27 @@ public class DescriptionTable extends JTable{
 		for(int i=0; i< this.getColumnCount(); i++){
 			switch(i){
 			case 0:
+				this.getColumnModel().getColumn(i).setPreferredWidth(100);
+				break;
 			case 1:
+				this.getColumnModel().getColumn(i).setMinWidth(180);
+				this.getColumnModel().getColumn(i).setMaxWidth(Integer.MAX_VALUE);
+				break;
 			case 2:	
 			case 3:	
-				this.getColumnModel().getColumn(i).setPreferredWidth(380);
+				this.getColumnModel().getColumn(i).setPreferredWidth(100);
 				break;
 			case 4:
 			case 5:
 			case 6:
 			case 7:
 			case 8:	
-				this.getColumnModel().getColumn(i).setPreferredWidth(180);
+				this.getColumnModel().getColumn(i).setMinWidth(50);
+				this.getColumnModel().getColumn(i).setMaxWidth(50);
 				break;
 			case 9:	
-				this.getColumnModel().getColumn(i).setPreferredWidth(280);
+				this.getColumnModel().getColumn(i).setMinWidth(60);
+				this.getColumnModel().getColumn(i).setMaxWidth(60);
 				break;	
 			}
 		}
