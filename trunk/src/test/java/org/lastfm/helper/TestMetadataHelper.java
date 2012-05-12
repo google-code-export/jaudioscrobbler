f66f0df4ad9e38d928415090dc5bc4c79b88cc4e
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-05-12 21:52:52 +0000

    Validate when metadata is extracted from filename

--- trunk/src/test/java/org/lastfm/helper/TestMetadataHelper.java
+++ trunk/src/test/java/org/lastfm/helper/TestMetadataHelper.java
@@ -3,6 +3,7 @@ package org.lastfm.helper;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,12 +11,18 @@ import org.junit.Before;
 import org.junit.Test;
 import org.lastfm.metadata.Metadata;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 public class TestMetadataHelper {
 
 	@InjectMocks
 	private MetadataHelper metadataHelper = new MetadataHelper();
+
+	@Mock
+	private Metadata metadata;
+	@Mock
+	private File file;
 	
 	private List<Metadata> metadatas = new ArrayList<Metadata>();
 
@@ -28,12 +35,35 @@ public class TestMetadataHelper {
 	
 	@Test
 	public void shouldKnowIfSameAlbum() throws Exception {
-		Metadata metadata = mock(Metadata.class);
 		Metadata anotherMetadata = mock(Metadata.class);
 		when(metadata.getAlbum()).thenReturn(album);
 		when(anotherMetadata.getAlbum()).thenReturn(album);
 		
 		assertTrue(metadataHelper.isSameAlbum(metadatas));
 	}
-
+	
+	@Test
+	public void shouldExtractMetadataFromFileWhenDash() throws Exception {
+		String filename = "jenifer lopez - 9A - 112.mp3";
+		when(file.getName()).thenReturn(filename);
+		when(metadata.getFile()).thenReturn(file);
+		
+		metadataHelper.extractFromFileName(metadata);
+		
+		verify(metadata).setArtist("jenifer lopez ");
+		verify(metadata).setTitle(" 9A ");
+	}
+	
+	@Test
+	public void shouldExtractMetadataFromFileWhenNoDash() throws Exception {
+		String expectedName = "jenifer lopez";
+		String filename = "jenifer lopez.mp3";
+		when(file.getName()).thenReturn(filename);
+		when(metadata.getFile()).thenReturn(file);
+		
+		metadataHelper.extractFromFileName(metadata);
+		
+		verify(metadata).setArtist(expectedName);
+		verify(metadata).setTitle(expectedName);
+	}
 }
