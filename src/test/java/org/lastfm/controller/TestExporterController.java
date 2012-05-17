5ded4af1818bad0859468e4f22665a21305a0917
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-05-17 18:48:37 +0000

    Error and Success cases added when exporting metadata

---
+++ src/test/java/org/lastfm/controller/TestExporterController.java
@@ -0,0 +1,51 @@
+package org.lastfm.controller;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lastfm.action.ActionResult;
+import org.lastfm.helper.ExporterHelper;
+import org.lastfm.model.ExportPackage;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class TestExporterController {
+
+	@InjectMocks
+	private ExporterController exporterController = new ExporterController();
+	
+	@Mock
+	private ExportPackage exportPackage;
+	@Mock
+	private ExporterHelper exporterHelper;
+	
+	@Before
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldSendMetadata() throws Exception {
+		when(exporterHelper.export(exportPackage)).thenReturn(ActionResult.Exported);
+		
+		ActionResult result = exporterController.sendMetadata(exportPackage);
+		
+		verify(exporterHelper).export(exportPackage);
+		assertEquals(ActionResult.Exported, result);
+	}
+	
+	@Test
+	public void shouldReportErrorInSendingMetadata() throws Exception {
+		when(exporterHelper.export(exportPackage)).thenThrow(new IOException());
+		
+		ActionResult result = exporterController.sendMetadata(exportPackage);
+		
+		assertEquals(ActionResult.Error, result);
+	}
+
+}
