ae7ae8a9abde7e2f1d60e180d625d9774c92b358
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-05-18 19:46:24 +0000

    Avoid duplicate bean definitions in ApplicationContext

--- src/test/java/org/lastfm/helper/TestApplicationContextSingleton.java
+++ src/test/java/org/lastfm/helper/TestApplicationContextSingleton.java
@@ -4,7 +4,8 @@ import static org.junit.Assert.assertNotNull;
 
 import org.asmatron.messengine.engines.DefaultEngine;
 import org.junit.Test;
-import org.lastfm.controller.ScrobblerController;
+import org.lastfm.gui.LoginWindow;
+import org.lastfm.gui.MainWindow;
 import org.springframework.context.ConfigurableApplicationContext;
 
 public class TestApplicationContextSingleton {
@@ -13,9 +14,11 @@ public class TestApplicationContextSingleton {
 	public void shouldCreateAnApplicationContext() throws Exception {
 		ConfigurableApplicationContext applicationContext = ApplicationContextSingleton.getApplicationContext();
 		DefaultEngine defaultEngine = applicationContext.getBean(DefaultEngine.class);
-		ScrobblerController scrobblerController = applicationContext.getBean(ScrobblerController.class);
+		MainWindow mainWindow = applicationContext.getBean(MainWindow.class);
+		LoginWindow loginWindow = applicationContext.getBean(LoginWindow.class);
 		assertNotNull(defaultEngine);
-		assertNotNull(scrobblerController);
+		assertNotNull(mainWindow);
+		assertNotNull(loginWindow);
 	}
 
 }
