94afe80d9ee7361e0ac38aa9fa0fed0bcc61e48a
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-05-21 17:23:18 +0000

    CloseImageIcon tested

---
+++ trunk/src/test/java/org/lastfm/util/TestCloseImageIcon.java
@@ -0,0 +1,43 @@
+package org.lastfm.util;
+
+import static org.junit.Assert.*;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.lastfm.controller.service.ImageService;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class TestCloseImageIcon {
+
+	@InjectMocks
+	private CloseImageIcon closeImageIcon = new CloseImageIcon();
+	
+	@Mock
+	private ImageService imageService;
+	@Mock
+	private Image image;
+	
+	@Before
+	public void setup() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void shouldGetImageIcon() throws Exception {
+		when(imageService.readCloseImage()).thenReturn(image);
+		
+		ImageIcon imageIcon = closeImageIcon.getImageIcon();
+		
+		verify(imageService).readCloseImage();
+		assertTrue(closeImageIcon instanceof ImageIconBase);
+		assertNotNull(imageIcon);
+	}
+	
+}
