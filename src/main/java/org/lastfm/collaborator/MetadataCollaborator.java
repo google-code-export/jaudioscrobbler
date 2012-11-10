c5f3103c89086211e49ac599414f526903d8c3dd
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-11-10 21:01:13 +0000

    MetadataCollaborator added

---
+++ src/main/java/org/lastfm/collaborator/MetadataCollaborator.java
@@ -0,0 +1,26 @@
+package org.lastfm.collaborator;
+
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.lastfm.model.Metadata;
+
+public class MetadataCollaborator {
+
+	private List<Metadata> metadatas;
+
+	public void setMetadatas(List<Metadata> metadatas) {
+		this.metadatas = metadatas;
+	}
+
+	public String getArtist() {
+		boolean isTheSame = true;
+		for (int i=1; i< metadatas.size(); i++) {
+			if (!metadatas.get(i).getArtist().equals(metadatas.get(i-1).getArtist())){
+				isTheSame = false;
+			}
+		}
+		return isTheSame ? metadatas.get(0).getArtist() : StringUtils.EMPTY;
+	}
+
+}
