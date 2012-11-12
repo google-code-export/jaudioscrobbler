4517963bab07425da15582720059cc8f266a63d9
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2012-11-12 14:42:42 +0000

    Service test package updated

--- src/test/java/org/jas/service/TestMetadataService.java
+++ src/test/java/org/jas/service/TestMetadataService.java
@@ -201,7 +201,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-package org.lastfm.controller.service;
+package org.jas.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
