5d106a1ca9353e33efb70509955d171c717d7a20
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2013-01-11 16:14:35 +0000

    LoginWindow was set look&feel by hipecotech

--- src/main/java/org/jas/gui/LoginWindow.java
+++ src/main/java/org/jas/gui/LoginWindow.java
@@ -203,27 +203,29 @@
 */
 package org.jas.gui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.Color;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
+import javax.swing.Icon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
 
 import org.asmatron.messengine.annotations.EventMethod;
 import org.asmatron.messengine.engines.support.ViewEngineConfigurator;
 import org.jas.ApplicationState;
 import org.jas.action.Actions;
 import org.jas.event.Events;
+import org.jas.gui.util.SelectedTextForeground;
+import org.jas.gui.util.SynthFonts;
 import org.jas.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -231,40 +233,47 @@ import org.springframework.beans.factory.annotation.Autowired;
  * @understands UI components for logging window
  */
 
-public class LoginWindow {
+public final class LoginWindow extends JPanel {
+	private static final long serialVersionUID = -8335316093543211530L;
 	private JButton sendButton;
 	private JTextField usernameTextfield;
-	private JTextField passwordTextfield;
+	private JPasswordField passwordTextfield;
 	private JFrame frame;
 	private JPanel panel;
 	private static final String SEND_BUTTON_LABEL = "Login";
-	private static final Rectangle FRAME_BOUNDS = new Rectangle(300, 300, 300, 122);
+	private static final Rectangle FRAME_BOUNDS = new Rectangle(300, 300, 390, 180);
 	private static final int TEXT_COLUMNS = 10;
 	private JLabel usernameLabel;
 	private JLabel passwordLabel;
 	
 	private static final String USERNAME_TEXTFIELD_NAME = "loginTextField";
 	private static final String PASSWORD_TEXTFIELD_NAME = "passwordLoginField";
-	private static final Rectangle USERNAME_TEXTFIELD_BOUNDS = new Rectangle(478, 97, 220, 34);
-	private static final Rectangle PASSWORD_TEXTFIELD_BOUNDS = new Rectangle(478, 147, 220, 34);
+	private static final Rectangle USERNAME_LABEL_BOUNDS = new Rectangle(20, 30, 120, 22);
+	private static final Rectangle PASSWORD_LABEL_BOUNDS = new Rectangle(20, 70, 120, 22);
+	private static final Rectangle USERNAME_TEXTFIELD_BOUNDS = new Rectangle(140, 27, 220, 34);
+	private static final Rectangle PASSWORD_TEXTFIELD_BOUNDS = new Rectangle(140, 71, 220, 34);
 	private static final String SIGN_UP_BUTTON_NAME = "buttonCenterLoginCreate";
-	private static final Rectangle SIGN_UP_BUTTON_BOUNDS = new Rectangle(548, 318, 150, 34);
+	private static final Rectangle SIGN_UP_BUTTON_BOUNDS = new Rectangle(140, 114, 220, 34);
+	
+	private static final Color COLOR_SELECTION_TEXTFIELDS = SelectedTextForeground.SELECTED_FOREGROUND_COLOR;
+	private static final Icon BULLET_ICON = UIManager.getDefaults().getIcon("icons.createAccountBullet");
+	private static final int REQUIRE_ICON_TEXT_GAP = 2;
 	
 	@Autowired
 	private ViewEngineConfigurator configurator;
 	
 	public LoginWindow() {
-		doLayout();
+		initialize();
 	}
 	
-	private void doLayout(){
+	private void initialize(){
 		getFrame().add(getPanel());
 		getFrame().setVisible(false);
 		
 		getSendButton().addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), passwordTextfield.getText()));
+				configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), new String(passwordTextfield.getPassword())));
 			}
 		});
 		
@@ -272,7 +281,7 @@ public class LoginWindow {
 			
 			@Override
 			public void keyPressed(KeyEvent e) {
-				configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), passwordTextfield.getText()));
+				configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), new String(passwordTextfield.getPassword())));
 			}
 		});
 		
@@ -282,7 +291,7 @@ public class LoginWindow {
 			public void keyPressed(KeyEvent e) {
 				int key = e.getKeyCode();
 				if(key == KeyEvent.VK_ENTER){
-					configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), passwordTextfield.getText()));
+					configurator.getViewEngine().sendValueAction(Actions.LOGIN, new User(usernameTextfield.getText(), new String(passwordTextfield.getPassword())));
 				}
 			}
 		});
@@ -295,24 +304,13 @@ public class LoginWindow {
 	
 	private JPanel getPanel() {
 		if(panel == null){
-			panel = new JPanel(new GridBagLayout());
-			GridBagConstraints c = new GridBagConstraints();
-			c.gridx = 0;
-			c.gridy = 0;
-			panel.add(getUserNameLabel(), c);
-			c.gridx = 1;
-			c.gridy = 0;
-			panel.add(getUsernameTextfield(), c);
-			c.gridx = 0;
-			c.gridy = 1;
-			panel.add(getPasswordLabel(), c);
-			c.gridx = 1;
-			c.gridy = 1;
-			panel.add(getPasswordTextfield(), c);
-			c.insets = new Insets(0, 40, 0, 0);
-			c.gridx = 1;
-			c.gridy = 2;
-			panel.add(getSendButton(), c);
+			panel = new JPanel();
+			panel.setLayout(null);
+			panel.add(getUserNameLabel());
+			panel.add(getUsernameTextfield());
+			panel.add(getPasswordLabel());
+			panel.add(getPasswordTextfield());
+			panel.add(getSendButton());
 		}
 		return panel;
 	}
@@ -321,6 +319,10 @@ public class LoginWindow {
 		if(passwordLabel == null){
 			passwordLabel = new JLabel();
 			passwordLabel.setText(ApplicationState.PASSWORD_LABEL);
+			passwordLabel.setBounds(PASSWORD_LABEL_BOUNDS);
+			passwordLabel.setName(SynthFonts.PLAIN_FONT16_GRAY100_100_100);
+			passwordLabel.setIcon(BULLET_ICON);
+			passwordLabel.setIconTextGap(REQUIRE_ICON_TEXT_GAP);
 		}
 		return passwordLabel;
 	}
@@ -329,7 +331,10 @@ public class LoginWindow {
 		if(usernameLabel == null){
 			usernameLabel = new JLabel();
 			usernameLabel.setText(ApplicationState.USERNAME_LABEL);
-			
+			usernameLabel.setBounds(USERNAME_LABEL_BOUNDS);
+			usernameLabel.setName(SynthFonts.PLAIN_FONT16_GRAY100_100_100);
+			usernameLabel.setIcon(BULLET_ICON);
+			usernameLabel.setIconTextGap(REQUIRE_ICON_TEXT_GAP);
 		}
 		return usernameLabel;
 	}
@@ -339,6 +344,7 @@ public class LoginWindow {
 			usernameTextfield = new JTextField(TEXT_COLUMNS);
 			usernameTextfield.setBounds(USERNAME_TEXTFIELD_BOUNDS);
 			usernameTextfield.setName(USERNAME_TEXTFIELD_NAME);
+			usernameTextfield.setSelectionColor(COLOR_SELECTION_TEXTFIELDS);
 		}
 		return usernameTextfield;
 	}
@@ -348,6 +354,8 @@ public class LoginWindow {
 			passwordTextfield = new JPasswordField(TEXT_COLUMNS);
 			passwordTextfield.setBounds(PASSWORD_TEXTFIELD_BOUNDS);
 			passwordTextfield.setName(PASSWORD_TEXTFIELD_NAME);
+			passwordTextfield.setSelectionColor(COLOR_SELECTION_TEXTFIELDS);
+			passwordTextfield.setEchoChar('\u25CF');
 		}
 		return passwordTextfield;
 	}
