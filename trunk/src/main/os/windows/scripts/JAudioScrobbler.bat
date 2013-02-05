a1bed10f53d8902c0507362ad21a09711d5a9cf7
joseluis.delacruz@gmail.com <joseluis.delacruz@gmail.com@8463f722-03e2-11df-8b0e-05a2803efa1e>
2013-02-05 16:52:46 +0000

    Windows assembly scripts updated

--- trunk/src/main/os/windows/scripts/JAudioScrobbler.bat
+++ trunk/src/main/os/windows/scripts/JAudioScrobbler.bat
@@ -5,7 +5,7 @@ set ALL_MEM=128m
 set ALL_MAX_MEM=1024m
 set CLASSPATH=System\Jar;System\Jar\*
 set STARTUP_ARGS=-Xmx%ALL_MAX_MEM% -Xms%ALL_MEM% -Djava.library.path="%ALL_DIR%\System\Lib"
-set MAIN_CLASS=org.lastfm.Launcher
+set MAIN_CLASS=org.jas.Launcher
 
 echo java %STARTUP_ARGS% -cp %CLASSPATH% %MAIN_CLASS%
 
